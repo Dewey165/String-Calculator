95f4fcdfc6458b975ec369ba40ba15c53e62ccba
Dewey165 <agi2211@gmail.com>
2016-10-12 21:57:57 +0000

    Test Cases for 1 are done

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 
 public class Calculator {
+	private Scanner reader;
+	public int main()
+	{
+		System.out.print("Input for Calculator: ");
+		reader = new Scanner(System.in);
+		String input = reader.next();
+		System.out.println(add(input));
+		return 0;
+	}
 	public static int add(String text)
 	{
 		if(text.equals(""))
@@ -10,9 +20,21 @@ public class Calculator {
 		{
 			return Integer.parseInt(text);
 		}
-		else
+		else 
 		{
-			return 1;
+			int total = 0;
+			String[] inputs = splitNumbers(text);
+			//Taken from the Hannes Petursson in lecture..
+			for(String number : inputs)
+			{
+				total += Integer.parseInt(number);
+			}
+			return total;
 		}
+	}
+	//Taken from Hannes Petursson in lecture..
+	private static String[] splitNumbers(String numbers)
+	{
+		return numbers.split(",");
 	}
 }
--- src/Tests.java
+++ src/Tests.java
@@ -1,28 +1,51 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
 
 public class Tests {
 	//Kata Steps 1
 	@Test
-	public void testEmptyString()
+	public void TestingZeroNumber()
 	{
 		assertEquals(0, Calculator.add(""));
 	}
 	@Test
-	public void testOneNumber()
+	public void TestingOneNumber()
 	{
 		assertEquals(1, Calculator.add("1"));
 	}
 	@Test
-	public void testTwoNumbers()
+	public void TestingTwoNumbers()
 	{
 		assertEquals(3, Calculator.add("1,2"));
 	}
 	
 	@Test
-	public void testThreeNumbers()
+	public void TestingThreeNumbers()
 	{
-		assertEquals(0, Calculator.add("1,2,3"));
+		assertEquals(6, Calculator.add("1,2,3"));
+	}
+	//seen at http://stackoverflow.com/questions/1096650/why-doesnt-junit-provide-assertnotequals-methods
+	@Test
+	public void WorstCaseTestingZeroNumber()
+	{
+		assertThat(1, not(Calculator.add("")));
+	}
+	@Test
+	public void WorstCaseTestingOneNumber()
+	{
+		assertThat(2, not(Calculator.add("1")));
+	}
+	@Test
+	public void WorstCaseTestingTwoNumbers()
+	{
+		assertThat(2, not(Calculator.add("1,2")));
+	}
+	@Test
+	public void WorstCaseTestingThreeNumbers()
+	{
+		assertThat(2, not(Calculator.add("1,2,3")));
 	}
 	
 }
